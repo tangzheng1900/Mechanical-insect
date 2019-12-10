a29d74f736cc3208902293ce2d769db9b182426f
zgh <849080458@qq.com>
2019-12-10 16:38:18 +0800

    》更新最新内容

--- Mechanical_insect/app/admin/views.py
+++ Mechanical_insect/app/admin/views.py
@@ -8,10 +8,37 @@
 
 from . import admin
 from flask import render_template, redirect, url_for, flash, session, request,abort
-
+from app.admin.forms import LoginForm
+from app.models import Admin, User,  Oplog, Adminlog, Userlog, Auth, Role
+from functools import wraps
+from app import db, app
+from werkzeug.utils import secure_filename
+import os
+import uuid
+import datetime
 
 
 # 系统管理
-@admin.route("/")
+@admin.route("admin/")
 def index ():
     return render_template("admin/index.html")
+
+# 登录
+@admin.route("admin/login/", methods=["GET", "POST"])
+def login ():
+    form = LoginForm()
+    if form.validate_on_submit():
+        data = form.data
+        admin = Admin.query.filter_by(name=data["account"]).first()
+        if not admin.check_pwd(data["pwd"]):
+            flash("密码错误！", "err")
+            return redirect(url_for("admin.login"))
+        session["admin"] = data["account"]
+        session["admin_id"] = admin.id
+        adminlog = Adminlog(admin_id=admin.id, ip=request.remote_addr, )
+        userlog = Userlog(admin_id=admin.id, ip=request.remote_addr, )
+        db.session.add(adminlog,userlog)
+        db.session.commit()
+        flash("登陆成功！", "ok")
+        return redirect(request.args.get("next") or url_for("admin.index"))
+    return render_template("admin/login.html", form=form)
